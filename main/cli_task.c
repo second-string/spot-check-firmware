bacdd193c3ed85ff46858349ae737206c72dc6e3
dot4qu <brian.team.jr@gmail.com>
2022-03-26 15:39:57 -0700

    Implement separate command process task that pops commands off queue that serial task fills rather than locking serial task to run command callbacks

--- main/cli_task.c
+++ main/cli_task.c
@@ -1,6 +1,7 @@
 #include <string.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 #include "freertos/task.h"
 
 #include "driver/uart.h"
@@ -17,9 +18,21 @@
 #define CLI_TASK_PRIORITY (tskIDLE_PRIORITY)
 #define CLI_COMMAND_BUFFER_BYTES (128)
 
+#define CLI_CMD_PROCESS_TASK_PRIORITY (tskIDLE_PRIORITY)
+#define CLI_COMMAND_QUEUE_SIZE (12)
+
+typedef struct {
+    char  *cmd;
+    size_t len;
+} cli_command_t;
+
 static uart_handle_t *handle;
 static char          *command_buffer;
 static uint8_t        command_char_idx;
+
+static QueueHandle_t queue_handle;
+static StaticQueue_t queue_buffer;
+static uint8_t      *queue_data_buffer;
 
 // TaskHandle_t cli_task_handle;
 
@@ -35,15 +48,49 @@ static uint8_t        command_char_idx;
 
 static void cli_process_char(char c) {
     command_buffer[command_char_idx++] = c;
-    if (c == '\n' || c == '\r') {
-        command_buffer[command_char_idx++] = '\0';
 
-        char  *base = "processing cmd:";
-        char   full[strlen(base) + strlen(command_buffer) + 1];
-        size_t full_size = sprintf(full, "%s %s", base, command_buffer);
-        uart_write_bytes(handle->port, full, full_size);
+    // TODO :: handle backspace
+    // Echo
+    uart_write_bytes(handle->port, &c, 1);
+
+    if (c == '\n' || c == '\r') {
+        command_buffer[command_char_idx] = '\0';
+
+        // char  *base = "processing cmd:";
+        // char   full[strlen(base) + strlen(command_buffer) + 1];
+        // size_t full_size = sprintf(full, "%s %s", base, command_buffer);
+
+        uart_write_bytes(handle->port, "\r\n", 2);
+
+        // Either free on failure here or dequeuer responsible for freeing
+        char *cmd_copy = pvPortMalloc(command_char_idx * sizeof(char));
+        memcpy(cmd_copy, command_buffer, command_char_idx);
+        cli_command_t cmd = {
+            .cmd = cmd_copy,
+            .len = command_char_idx * sizeof(char),
+        };
+
+        // Queue will copy cmd struct values to internal queue items, no need to malloc it
+        BaseType_t rval = xQueueSendToBack(queue_handle, &cmd, pdMS_TO_TICKS(10));
+        if (!rval) {
+            vPortFree(cmd_copy);
+        }
 
         command_char_idx = 0;
+    }
+}
+
+/*
+ * Task to pop commands off the queue and execute them. Seperates long or blocking command handlers from serial rx
+ */
+static void cli_process_command(void *args) {
+    QueueHandle_t queue = args;
+
+    BaseType_t    rval = pdFALSE;
+    cli_command_t cmd;
+    while (1) {
+        rval = xQueueReceive(queue, &cmd, portMAX_DELAY);
+        assert(rval);
     }
 }
 
@@ -59,14 +106,26 @@ void cli_task_init(uart_handle_t *uart_handle) {
     command_buffer = pvPortMalloc(CLI_COMMAND_BUFFER_BYTES * sizeof(char));
     assert(command_buffer);
     command_char_idx = 0;
+
+    queue_data_buffer = pvPortMalloc(CLI_COMMAND_QUEUE_SIZE * sizeof(cli_command_t));
+    assert(queue_data_buffer);
+    queue_handle = xQueueCreateStatic(CLI_COMMAND_QUEUE_SIZE, sizeof(cli_command_t), queue_data_buffer, &queue_buffer);
+    assert(queue_handle);
 }
 
 void cli_task_start() {
     BaseType_t rval = xTaskCreate(uart_generic_rx_task,
-                                  "CLI",
+                                  "CLI UART RX",
                                   SPOT_CHECK_MINIMAL_STACK_SIZE_BYTES * 2,
                                   handle,
                                   CLI_TASK_PRIORITY,
                                   NULL);
     assert(rval);
+
+    rval = xTaskCreate(cli_process_command,
+                       "CLI cmd process",
+                       SPOT_CHECK_MINIMAL_STACK_SIZE_BYTES * 2,
+                       queue_handle,
+                       CLI_CMD_PROCESS_TASK_PRIORITY,
+                       NULL);
 }
