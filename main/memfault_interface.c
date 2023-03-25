dc3b68d49cf068737160e329be7779fa596c9075
dot4qu <brian.team.jr@gmail.com>
2023-03-25 13:22:11 +0100

    Include all task stack and heap stats in memfault heartbeat

--- main/memfault_interface.c
+++ main/memfault_interface.c
@@ -3,7 +3,10 @@
 #include "memfault/components.h"
 #include "memfault/http/http_client.h"
 
+#include "cli_task.h"
 #include "log.h"
+#include "ota_task.h"
+#include "scheduler_task.h"
 
 #define TAG SC_TAG_MFLT_INTRFC
 
@@ -50,4 +53,28 @@ bool memfault_interface_post_data() {
     }
 
     return success;
+}
+
+/*
+ * Memfault weak function, override here to bundle custom metrics every time heartbeat elapsed and data sent
+ */
+void memfault_metrics_heartbeat_collect_data() {
+    size_t      total                 = heap_caps_get_total_size(MALLOC_CAP_8BIT);
+    size_t      free                  = heap_caps_get_free_size(MALLOC_CAP_8BIT);
+    size_t      min_free              = heap_caps_get_minimum_free_size(MALLOC_CAP_8BIT);
+    size_t      largest_block         = heap_caps_get_largest_free_block(MALLOC_CAP_8BIT);
+    UBaseType_t cli_total_words       = cli_task_get_stack_high_water();
+    UBaseType_t ota_total_words       = ota_task_get_stack_high_water();
+    UBaseType_t scheduler_total_words = scheduler_task_get_stack_high_water();
+
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(total_heap_bytes), total);
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(free_heap_bytes), free);
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(low_watermark_heap_bytes), min_free);
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(largest_free_heap_block_bytes), largest_block);
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(cli_task_high_water_stack_bytes),
+                                            cli_total_words * sizeof(uint32_t));
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(ota_task_high_water_stack_bytes),
+                                            ota_total_words * sizeof(uint32_t));
+    memfault_metrics_heartbeat_set_unsigned(MEMFAULT_METRICS_KEY(scheduler_task_high_water_stack_bytes),
+                                            scheduler_total_words * sizeof(uint32_t));
 }
