ba49211b57655a051d62c7860b4af6ad49d66a40
second-string <brian.team.jr@gmail.com>
2024-01-03 02:22:39 -0600

    update configuration script to properly read out new config fields (mode, both customs), and set both default fields and mode-specific ones

--- configuration_app/spot_check_configure.py
+++ configuration_app/spot_check_configure.py
@@ -25,40 +25,7 @@ banner = """
         |_|
 """
 
-def configure():
-    db_conn = sqlite3.connect("timezones.db")
-    cursor = db_conn.cursor()
-
-    print("Searching for Spot Check device on the network...")
-    success = False
-    while not success:
-        try:
-            res = requests.get("http://spot-check.local./health", headers={"Content-type": "application/json"}, timeout=0.2)
-            res.raise_for_status()
-            success = True
-        except Exception as e:
-            success = False
-            print("Could not find Spot Check device on the network. Make sure you you have connected the device to the network using step 1 in the main menu, and that this computer is connected to the same network.")
-            print("Retrying in 5 seconds...")
-            sleep(5)
-
-    print("Found device!")
-    print()
-    print("Fetching device current configuration...")
-    try:
-        res = requests.get("http://spot-check.local./current_configuration", headers={"Content-type": "application/json"}, timeout=0.2)
-        res.raise_for_status()
-    except Exception as e:
-        raise e
-
-    current_config = res.json()
-
-    print()
-    print("Current configuration stored on device:")
-    print(f"Spot name: {current_config['spot_name']}")
-    print(f"Timezone: {current_config['tz_display_name']}")
-    print()
-
+def configure_weather_mode(current_config):
     spot_name = ""
     spot_uid = ""
     spot_lat = ""
@@ -92,6 +59,83 @@ def configure():
         if spot_name == "":
             print()
 
+    return {
+        "spot_name": spot_name,
+        "spot_lat": spot_lat,
+        "spot_lon": spot_lon,
+        "spot_uid": spot_uid,
+    }
+
+
+def configure_custom_mode(current_config):
+    custom_screen_url = ""
+    custom_update_interval_secs = 0
+
+    temp_custom_screen_url = input(f"Enter a URL to an API endpoint that returns a custom image for the screen (press enter keep as '{current_config['custom_screen_url']}'): ")
+    if not temp_custom_screen_url or temp_custom_screen_url == "":
+        custom_screen_url = current_config["custom_screen_url"]
+    else:
+        custom_screen_url = temp_custom_screen_url
+
+    temp_update_interval_secs = input(f"Enter the length of time the device should wait in between requests to the API endpoint in seconds (press enter to keep as '{current_config['custom_update_interval_secs']}'): ")
+    if not temp_update_interval_secs or temp_update_interval_secs == "":
+        custom_update_interval_secs = current_config["custom_update_interval_secs"]
+    else:
+        custom_update_interval_secs = int(temp_update_interval_secs)
+
+        return {
+            "custom_screen_url": custom_screen_url,
+            "custom_update_interval_secs": custom_update_interval_secs,
+        }
+
+def configure():
+    db_conn = sqlite3.connect("timezones.db")
+    cursor = db_conn.cursor()
+
+    # print("Searching for Spot Check device on the network...")
+    # success = False
+    # while not success:
+    #     try:
+    #         res = requests.get("http://spot-check.local./health", headers={"Content-type": "application/json"}, timeout=0.2)
+    #         res.raise_for_status()
+    #         success = True
+    #     except Exception as e:
+    #         success = False
+    #         print("Could not find Spot Check device on the network. Make sure you you have connected the device to the network using step 1 in the main menu, and that this computer is connected to the same network.")
+    #         print("Retrying in 5 seconds...")
+    #         sleep(5)
+
+    print("Found device!")
+    print()
+    print("Fetching device current configuration...")
+    # try:
+    #     res = requests.get("http://spot-check.local./current_configuration", headers={"Content-type": "application/json"}, timeout=0.2)
+    #     res.raise_for_status()
+    # except Exception as e:
+    #     raise e
+
+    # current_config = res.json()
+    current_config = {
+        "tz_display_name": "",
+        "operating_mode": "custom",
+        "spot_name": "",
+        "custom_screen_url": "",
+        "custom_update_interval_secs": "0",
+    }
+
+    print()
+    print("Current configuration stored on device:")
+    print(f"Timezone: {current_config['tz_display_name']}")
+    print(f"Device mode: {current_config['operating_mode']}")
+    if current_config['operating_mode'] == "weather":
+        print(f"Spot name: {current_config['spot_name']}")
+    elif current_config['operating_mode'] == "custom":
+        print(f"Custom external URL: {current_config['custom_screen_url']}")
+        print(f"Screen update interval (seconds): {int(current_config['custom_update_interval_secs'])}")
+    else:
+        assert 0
+    print()
+
     # TODO :: include 'manual' option to manually set time)
     tz_str = ""
     tz_display_name = ""
@@ -117,13 +161,30 @@ def configure():
         if tz_str == "":
             print()
 
+    choice = None
+    mode = ""
+    print()
+    print("Select device mode:")
+    while choice != "1" and choice != "2":
+        print("1) Weather mode: display the time, current weather conditions, and two weather charts on the screen")
+        print("2) Custom mode: display custom data on the screen hosted on your own external API")
+        choice = input("(1/2): ")
+        print()
+
+    extra_vals = {}
+    if choice == "1":
+        mode = "weather"
+        extra_vals = configure_weather_mode(current_config)
+    elif choice == "2":
+        mode = "custom"
+        extra_vals = configure_custom_mode(current_config)
+
+    print(extra_vals)
     body = {
-        "spot_name": spot_name,
-        "spot_lat": spot_lat,
-        "spot_lon": spot_lon,
-        "spot_uid": spot_uid,
+        "operating_mode": mode,
         "tz_str": tz_str,
         "tz_display_name": tz_display_name,
+        # TODO :: how to merge extra_vals dict with this one
     }
 
     print()
